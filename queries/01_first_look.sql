7f75ca03d8ecbcb12fa0f2e1924812c45020641b
TheLearningSloth <tina.luu.trinh@gmail.com>
2026-04-13 09:26:27 -0500

    Add first SQL query - peek at data

---
+++ queries/01_first_look.sql
@@ -0,0 +1,11 @@
+-- =============================================
+-- First query: Just peek at the data
+-- Goal: Get comfortable seeing what the Superstore sales data looks like
+-- Date: April 13, 2026
+-- =============================================
+
+-- Simple SELECT to show the first 10 rows
+-- This is exactly like the very first thing you probably did in your LinkedIn course
+SELECT *
+FROM superstore_sales
+LIMIT 10;
